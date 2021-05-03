033235c3afd7042d082d814e8bc064931d6cccc9
xyryxkichiro <81612007+xyryxkichiro@users.noreply.github.com>
2021-05-03 15:58:22 +0800

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,11 @@
+function xyryx(){
+    var txt;
+    if(confirm("Are you Sure?")){
+        txt="Thank You!";
+        alert("Thank you for sending us a message, please stay tune.");
+    } else {
+        txt="You cancelled";
+        alert("You cancelled");
+    }
+
+}
